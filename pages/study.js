51288f57d9ec7556cff632281792f958c5d2f553
dmartin305 <dmartin305@gatech.edu>
2022-04-29 16:39:29 -0400

    create skeleton webgazer wrapper

--- pages/study.js
+++ pages/study.js
@@ -1,9 +1,12 @@
 import ConditionLayout from "../comps/ConditionLayout";
+import WebGazerWrapper from "../comps/WebGazerWrapper";
 
 export default function Study() {
-    return( 
-        <>
-            <ConditionLayout/>
-        </>
-    );
+  return (
+    <>
+      <WebGazerWrapper>
+        <ConditionLayout />
+      </WebGazerWrapper>
+    </>
+  );
 }
